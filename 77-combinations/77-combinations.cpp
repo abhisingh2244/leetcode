b8b5ae1b0a639403d9f7692d0f6d377b4ffd848c
abhisingh2244 <72192470+abhisingh2244@users.noreply.github.com>
2022-08-05 09:50:06 +0530

    Time: 476 ms (19.26%), Space: 149.7 MB (18.63%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,18 +1,30 @@
 class Solution {
+
 public:
-    vector<vector<int>> combine(int n, int k) {
-       vector<vector<int>> result;
-		int i = 0;
-		vector<int> p(k, 0);
-		while (i >= 0) {
-			p[i]++;
-			if (p[i] > n) --i;
-			else if (i == k - 1) result.push_back(p);
-			else {
-			    ++i;
-			    p[i] = p[i - 1];
-			}
-		}
-		return result;
+void solve(vector<vector<int>> &ans,vector<int> &v,vector<int> temp,int ind,int k){
+    
+    if(v.size()==k){
+        ans.push_back(v);
+        return;
     }
+    
+    for(int i=ind;i<temp.size();i++){
+        v.push_back(temp[i]);
+        solve(ans,v,temp,i+1,k);
+        v.pop_back();
+    }
+    
+}
+vector<vector<int>> combine(int n, int k) {
+    
+    vector<int> temp(n);
+    for(int i=0;i<n;i++)
+        temp[i]=i+1;
+    vector<vector<int>> ans;
+    vector<int> v;
+    int ind=0;
+    
+    solve(ans,v,temp,ind,k);
+    return ans;
+}
 };
