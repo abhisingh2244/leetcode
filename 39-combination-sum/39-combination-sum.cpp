f119f5dc0a9da4b59a4d23424324ff664d789475
abhisingh2244 <72192470+abhisingh2244@users.noreply.github.com>
2022-08-05 08:50:24 +0530

    Time: 9 ms (75.76%), Space: 11.3 MB (53.82%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+//global values shared between both function
+vector<vector<int>>result;
+vector<int>current;
+int sum;
+
+void function(vector<int>& candidates,int target,int index)
+{
+    if(sum>target)return ;//base case if sum is greater then target then return 
+    
+    if(sum==target){
+        result.push_back(current);//id sum is equal to target then just add current to result
+    }
+    for(int i=index;i<candidates.size();i++){
+        sum+=candidates[i];//and current value to sum
+        current.push_back(candidates[i]);//and current value to current vector
+        function(candidates,target,i);//again reccure for same index i
+        sum-=candidates[i];//back track mean remove value that previously added
+        current.pop_back();//remove the value that previously added to current 
+    }      
+}
+
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    sum=0; // This sum is global you can see it on the top of code just a clarify if you have any doubt
+    function(candidates,target,0);//i make result current and sum global to reduce the size of function for simplecity
+    return result;//This result is also global 
+}
+};
