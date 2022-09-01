a96e0984b0cedd3000498de34c76e5bdbf7d5b99
abhisingh2244 <72192470+abhisingh2244@users.noreply.github.com>
2022-09-01 14:00:58 +0530

    Time: 12 ms (19.03%), Space: 12.1 MB (36.47%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-       next_permutation(begin(nums), end(nums));   
+       next_permutation(nums.begin(), nums.end());   
     }
 };
