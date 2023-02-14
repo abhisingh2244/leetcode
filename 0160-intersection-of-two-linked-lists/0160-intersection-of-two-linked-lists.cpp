099b8bc4607ad9cb439596006d1d27b5462c7b9d
abhisingh2244 <72192470+abhisingh2244@users.noreply.github.com>
2023-02-14 23:45:58 +0530

    Time: 52 ms (56.21%), Space: 17 MB (18.58%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,17 +8,19 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *head1, ListNode *head2) {
-         while(head2 != NULL) {
-        ListNode* temp = head1;
-        while(temp != NULL) {
-            //if both nodes are same
-            if(temp == head2) return head2;
-            temp = temp->next;
+    ListNode *getIntersectionNode(ListNode *h2, ListNode *h1) {
+        unordered_set<ListNode*>mp;
+        while(h2!=NULL)
+        {
+        mp.insert(h2); 
+            h2=h2->next;
+            
         }
-        head2 = head2->next;
-    }
-    //intersection is not present between the lists return null
-    return NULL;
+        while(h1!=NULL)
+        {
+        if(mp.find(h1)!=mp.end())return h1;
+            h1=h1->next;
+            }
+        return NULL;
     }
 };
