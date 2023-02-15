9df6972898936c0925be62e699c7017a6b507389
abhisingh2244 <72192470+abhisingh2244@users.noreply.github.com>
2023-02-15 20:24:45 +0530

    Time: 231 ms (83.17%), Space: 114.3 MB (73.41%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,41 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode*reverseLL(ListNode*head){
-        if(head==NULL || head->next==NULL){
-            return head;
-        }
-        ListNode*temp=reverseLL(head->next);
-        head->next->next=head;
-        head->next=NULL;
-        return temp;
-    }
-    ListNode*mid(ListNode*head){
-        ListNode*slow=head;
-        ListNode*fast=head->next;
-        while(fast!=NULL && fast->next!=NULL){
-            slow=slow->next;
+    ListNode* middle(ListNode* head)
+    {
+    ListNode *fast=head;
+        ListNode* slow=head;
+        while(fast->next!=NULL and fast->next->next!=NULL)
+        {
+        slow=slow->next;
             fast=fast->next->next;
         }
         return slow;
+    
+    }
+    ListNode *reverse(ListNode* temp)
+    {
+    ListNode* curr=temp;
+        ListNode * prev=NULL;
+        while(curr!=NULL)
+        {
+        ListNode* forward=curr->next;
+        curr->next=prev;
+            prev=curr;
+        curr=forward;}
+        return prev;
     }
     bool isPalindrome(ListNode* head) {
-        if(head==NULL){
-            return true;
-        }
-        if(head->next==NULL){
-            return true;
-        }
-        ListNode*midnode=mid(head);
-        midnode=midnode->next;
-        ListNode*reversedmid=reverseLL(midnode);
-        ListNode*temp=head;
-        while(temp!=midnode && reversedmid!=NULL){
-            if(temp->val!=reversedmid->val){
-                return false;
-            }
-            temp=temp->next;
-            reversedmid=reversedmid->next;
-        }
-        return true;
+        if(head==NULL or head->next==NULL)return true;
+        ListNode* mid=middle( head);
+        mid=mid->next;
+        ListNode* rev=reverse(mid);
+        
+        ListNode *dummy=head;
+        while(rev!=NULL and dummy!=mid)
+        {
+        if(rev->val!=dummy->val)return false;
+            rev=rev->next;
+            dummy=dummy->next;
+        }return true;
     }
 };
