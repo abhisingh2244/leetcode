4e34bf07d36daf9d1c06eb751cf743d0f2e4fbd2
abhisingh2244 <72192470+abhisingh2244@users.noreply.github.com>
2023-04-19 13:20:05 +0530

    Added solution - LeetHub

---
+++ Wifi Range - GFG/wifi-range.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    bool wifiRange(int N, string S, int X){
+        // code here
+         int j=-1;
+        for(int i=0;i<N;i++)
+        if(S[i]=='1') if((i-X)-1<=j)  j=i+X; else return false;
+        if(j<N-1) return false;
+        else return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int T;
+    cin >> T;
+    while(T--){
+        int N, X;
+        string S;
+        cin >> N >> X >> S;
+        Solution obj;
+        cout << obj.wifiRange(N, S, X) << "\n";
+    }
+}
+
+// } Driver Code Ends
