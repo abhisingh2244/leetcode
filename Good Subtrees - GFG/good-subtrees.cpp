4723b86a0583fcff275499c2770d32be9deadf0c
abhisingh2244 <72192470+abhisingh2244@users.noreply.github.com>
2023-05-05 23:21:35 +0530

    Added solution - LeetHub

---
+++ Good Subtrees - GFG/good-subtrees.cpp
@@ -0,0 +1,142 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+}; 
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    set<int> sol(Node* root,int k,int &cnt){
+        if(!root) {
+            return {};
+        }
+        set<int> l = sol(root->left,k,cnt);
+        set<int> r = sol(root->right,k,cnt);
+        
+        // process
+        set<int>total;
+        total.insert(root->data);
+        // left
+        for(auto it:l){
+            total.insert(it);
+        }
+        // right
+        for(auto it:r){
+            total.insert(it);
+        }
+        
+        
+        if(total.size()<=k) cnt++;
+        return total;
+    }
+    int goodSubtrees(Node *root,int k){
+        // Code here
+        int cnt = 0;
+        set<int> distinct = sol(root,k,cnt);
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        if(i==0){
+            string s;
+            getline(cin,s);
+        }
+        string str;
+        getline(cin,str);
+        int k;
+        cin>>k;
+        string newline;
+        getline(cin,newline);
+        Node* root = buildTree(str);
+        Solution ob;
+        cout<<ob.goodSubtrees(root,k)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
