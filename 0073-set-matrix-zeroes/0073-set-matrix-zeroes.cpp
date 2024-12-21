2c4ade134f046fe23df00e5ec036be2cb2c9c84f
abhisingh2244 <72192470+abhisingh2244@users.noreply.github.com>
2024-12-21 18:59:14 +0530

    Time: 0 ms (100.00%), Space: 18.7 MB (20.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,32 +1,45 @@
 class Solution {
 public:
-   
-void setZeroes(vector < vector < int >> & matrix) {
-  int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
-  for (int i = 0; i < rows; i++) {
-    //checking if 0 is present in the 0th column or not
-    if (matrix[i][0] == 0) col0 = 0;
-    for (int j = 1; j < cols; j++) {
-      if (matrix[i][j] == 0) {
-        matrix[i][0] = 0;
-        matrix[0][j] = 0;
-      }
+    void setZeroes(vector<vector<int>>& matrix) {
+        int row =matrix.size(), col=matrix[0].size();
+        vector<vector<int>> duplicate(row);
+        for(int i=0;i<row;i++)
+        {
+        for(int j=0;j<col;j++)
+        {
+            //check if any element is equal to 0
+            if(matrix[i][j]==0)
+            {
+                //Make replace all the element in column with some random no.
+                for(int k=0;k<col;k++)
+                { if(matrix[i][k]!=0)
+                       matrix[i][k]=-999;
+                }
+            }
+        
+        }
+        }
+         for(int i=0;i<row;i++)
+        {
+        for(int j=0;j<col;j++)
+        {
+            //check if any element is equal to 0
+            if(matrix[i][j]==0)
+            {
+                //Make replace all the element in row with some random no.
+                for(int k=0;k<row;k++)
+                { if(matrix[k][j]!=0)
+                       matrix[k][j]=-999;
+                }
+            }
+        
+        }
+        }
+        for(int i=0;i<row;i++)
+        {
+        for(int j=0;j<col;j++)
+        {
+         if(matrix[i][j]==-999) matrix[i][j]=0;
+        }}
     }
-  }
-  //traversing in the reverse direction and
-  //checking if the row or col has 0 or not
-  //and setting values of matrix accordingly.
-  for (int i = rows - 1; i >= 0; i--) {
-    for (int j = cols - 1; j >= 1; j--) {
-      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-        matrix[i][j] = 0;
-      }
-    }
-    if (col0 == 0) {
-      matrix[i][0] = 0;
-    }
-
-  }
-
-}
 };
